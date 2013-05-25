4b7896f1d4daa36775752a963dcd9ac7c342f0b8
Sebastian Chande <chande@cs.fsu.edu>
2013-05-25 17:37:58 -0400

    added comments to 20.rb

--- 20.rb
+++ 20.rb
@@ -1,9 +1,16 @@
-factorial = 100
+=begin
+  Main idea: Calculate 100! using Ruby's downto function,
+             and iterate through the result, adding each digit
+             to a running sum as it is encountered.
+=end
 
+#calculate 100!
+factorial = 100
 99.downto(1){|x|
   factorial = x * factorial
 }
 
+#calculate sum of digits in the result of 100!
 total = 0
 for x in factorial.to_s.split("")
   total = total + x.to_i
